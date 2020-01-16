64f0d32deb3b5b4875bc0826a541d6268093b14c
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 17:20:05 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,18 +13,22 @@ def third_challenge
         name: "Romeo",
         age: "15",
         status: "alive"
-        #your key/value pairs here
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lord Capulet",
+        age: "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
+        name: "Juliet",
+        age: "15"
+        status: "alive"
         #your key/value pairs here
         },
       :heroine_friends => []
